ab9410dfd31abdf86f3099084103f91a4ecd9e0c
Clar1fy <timplifier@gmail.com>
2022-09-20 23:24:52 +0600

    Initial commit

---
+++ app/src/main/java/com/timberta/walletleaks/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.timberta.walletleaks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
