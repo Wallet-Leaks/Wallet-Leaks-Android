3adc4ffebb447a39130fa5cb0f5fafc815752427
Clar1fy <timplifier@gmail.com>
2022-09-21 02:17:22 +0600

    added baserepository

---
+++ data/src/main/kotlin/com/timberta/walletleaks/data/repositories/base/BaseRepository.kt
@@ -0,0 +1,21 @@
+package com.timberta.walletleaks.data.repositories.base
+
+import com.timberta.walletleaks.domain.either.Either
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+
+internal fun <T> makeNetworkRequest(
+    gatherIfSucceed: ((T) -> Unit)? = null,
+    request: suspend () -> T
+) =
+    flow<Either<String, T>> {
+        request().also {
+            gatherIfSucceed?.invoke(it)
+            emit(Either.Right(value = it))
+        }
+    }.flowOn(Dispatchers.IO).catch { exception ->
+        emit(Either.Left(value = exception.localizedMessage ?: "Error Occurred!"))
+    }
