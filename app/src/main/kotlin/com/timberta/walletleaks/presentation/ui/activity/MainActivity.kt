457f189ca59b596982ce1f703b14cbfe0cb61568
albertik001 <albert.nurlanovv@mail.ru>
2022-09-30 01:28:02 +0600

    Main activity_main.xml

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/activity/MainActivity.kt
@@ -2,13 +2,26 @@ package com.timberta.walletleaks.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timberta.walletleaks.R
-import dagger.hilt.android.AndroidEntryPoint
+import com.timberta.walletleaks.databinding.ActivityMainBinding
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.itemIconTintList = null
+        setupWithNavController(binding.bottomNavigation, navController)
     }
 }
