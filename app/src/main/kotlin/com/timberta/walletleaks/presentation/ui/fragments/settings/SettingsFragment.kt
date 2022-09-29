d8587add9e8fcf385f7f0ab523addc9998a2cbff
albertik001 <albert.nurlanovv@mail.ru>
2022-09-30 01:08:03 +0600

    фрагмент сеттингс

---
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/settings/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package com.timberta.walletleaks.presentation.ui.fragments.settings
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.timberta.walletleaks.R
+import com.timberta.walletleaks.databinding.FragmentSettingsBinding
+import com.timberta.walletleaks.presentation.base.BaseFragment
+
+class SettingsFragment :
+    BaseFragment<FragmentSettingsBinding, SettingsViewModel>(R.layout.fragment_settings) {
+
+    override val binding by viewBinding(FragmentSettingsBinding::bind)
+    override val viewModel by viewModels<SettingsViewModel>()
+}
