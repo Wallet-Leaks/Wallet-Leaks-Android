745b114968fcc73c94da219515c0438999fbdd83
albertik001 <albert.nurlanovv@mail.ru>
2022-09-30 01:09:22 +0600

    fragment coin list

---
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/coinList/CoinListFragment.kt
@@ -0,0 +1,17 @@
+package com.timberta.walletleaks.presentation.ui.fragments.coinList
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.timberta.walletleaks.R
+import com.timberta.walletleaks.databinding.FragmentCoinListBinding
+import com.timberta.walletleaks.presentation.base.BaseFragment
+import org.koin.dsl.module
+
+
+class CoinListFragment :
+    BaseFragment<FragmentCoinListBinding, CoinListViewModel>(R.layout.fragment_coin_list) {
+
+    override val binding by viewBinding(FragmentCoinListBinding::bind)
+    override val viewModel by viewModels<CoinListViewModel>()
+
+}
