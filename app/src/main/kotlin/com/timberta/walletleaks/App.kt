543f43de4d92297a85408057f04120c6cd6ecc28
albertik001 <albert.nurlanovv@mail.ru>
2022-09-30 15:08:28 +0600

    указал запуск с коина

--- app/src/main/kotlin/com/timberta/walletleaks/App.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/App.kt
@@ -1,5 +1,17 @@
 package com.timberta.walletleaks
 
 import android.app.Application
+import com.timberta.walletleaks.data.di.dataModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(dataModule)
+        }
+    }
+}
