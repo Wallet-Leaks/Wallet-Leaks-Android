44569fa832e0c1133930b33529f8e68d7c86c4bb
albertik001 <albert.nurlanovv@mail.ru>
2022-09-30 15:08:39 +0600

    проверял работу префса

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeFragment.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeFragment.kt
@@ -3,11 +3,18 @@ package com.timberta.walletleaks.presentation.ui.fragments.main.home
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timberta.walletleaks.R
+import com.timberta.walletleaks.data.db.preferences.AccessPremiumPreferencesManager
 import com.timberta.walletleaks.databinding.FragmentHomeBinding
 import com.timberta.walletleaks.presentation.base.BaseFragment
+import org.koin.android.ext.android.inject
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
 
     override val binding by viewBinding(FragmentHomeBinding::bind)
     override val viewModel by viewModels<HomeViewModel>()
+    private val accessPremiumPreferencesManager: AccessPremiumPreferencesManager by inject()
+
+    override fun initialize() {
+        accessPremiumPreferencesManager.isShowAccessPremium = true
+    }
 }
