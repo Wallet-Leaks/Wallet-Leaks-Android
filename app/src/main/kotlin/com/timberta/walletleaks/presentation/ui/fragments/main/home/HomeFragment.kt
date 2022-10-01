0cf799d9c69eba989726f20bbcf29a46d9591084
albertik001 <albert.nurlanovv@mail.ru>
2022-10-01 22:32:48 +0600

    тест диалог

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeFragment.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.timberta.walletleaks.presentation.ui.fragments.main.home
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timberta.walletleaks.R
 import com.timberta.walletleaks.data.db.preferences.AccessPremiumPreferencesManager
 import com.timberta.walletleaks.databinding.FragmentHomeBinding
 import com.timberta.walletleaks.presentation.base.BaseFragment
+import com.timberta.walletleaks.presentation.extensions.navigateSafely
 import org.koin.android.ext.android.inject
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
@@ -15,6 +17,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
     private val accessPremiumPreferencesManager: AccessPremiumPreferencesManager by inject()
 
     override fun initialize() {
-
+        findNavController().navigateSafely(R.id.action_homeFragment_to_premiumPurchaseFragment)
     }
 }
