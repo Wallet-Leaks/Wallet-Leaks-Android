e5a19fcc0434fb9c2def4e7bb00369586a249786
albertik001 <albert.nurlanovv@mail.ru>
2022-10-06 23:22:57 +0600

    по нажатии кнопки купить переходит на телеграмм акк

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/premium/dialog/PremiumPurchaseDialog.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/premium/dialog/PremiumPurchaseDialog.kt
@@ -1,12 +1,17 @@
 package com.timberta.walletleaks.presentation.ui.fragments.main.premium.dialog
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.text.SpannableString
 import android.text.style.AbsoluteSizeSpan
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timberta.walletleaks.R
 import com.timberta.walletleaks.databinding.DialogPremiumPurchaseBinding
 import com.timberta.walletleaks.presentation.base.BaseDialogFragment
+import com.timberta.walletleaks.presentation.extensions.showShortDurationSnackbar
 
 
 class PremiumPurchaseDialog :
@@ -26,6 +31,18 @@ class PremiumPurchaseDialog :
     override fun setupListeners() {
         binding.btnCloseDialog.setOnClickListener {
             dialog?.cancel()
+        }
+        binding.btnBuyPremium.setOnClickListener {
+            findNavController().navigateUp()
+            val username = getString(R.string.telegram_account)
+            val telegramIntent = Intent(
+                Intent.ACTION_VIEW, Uri.parse("https://t.me/$username")
+            ).setPackage("org.telegram.messenger")
+            try {
+                startActivity(telegramIntent)
+            } catch (e: ActivityNotFoundException) {
+                showShortDurationSnackbar(getString(R.string.telegram_not_installed))
+            }
         }
     }
 
