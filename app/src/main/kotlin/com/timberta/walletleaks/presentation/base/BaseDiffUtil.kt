56aa6866130cb40ecda80205e5c44a284bdb9bf3
albertik001 <albert.nurlanovv@mail.ru>
2022-10-11 23:29:07 +0600

    diffUtil на проверку адреса

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/base/BaseDiffUtil.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/base/BaseDiffUtil.kt
@@ -3,7 +3,7 @@ package com.timberta.walletleaks.presentation.base
 import androidx.recyclerview.widget.DiffUtil
 
 interface BaseDiffModel<T> {
-    val id: T
+    val address: T?
     override fun equals(other: Any?): Boolean
 }
 
@@ -11,7 +11,7 @@ class BaseDiffUtil<T : BaseDiffModel<S>, S> :
     DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
-        return oldItem.id == newItem.id
+        return oldItem.address == newItem.address
     }
 
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
