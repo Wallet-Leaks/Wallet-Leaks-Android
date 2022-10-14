8cbda56902f57a49af32dae236280ef6b83f006b
Clar1fy <timplifier@gmail.com>
2022-10-14 23:16:18 +0600

    added fetchCoinsUseCase in constructor, added a function to fetch coins from the backend

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/coinList/CoinListViewModel.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/coinList/CoinListViewModel.kt
@@ -1,6 +1,16 @@
 package com.timberta.walletleaks.presentation.ui.fragments.main.coinList
 
+import com.timberta.walletleaks.domain.useCases.FetchCoinsUseCase
 import com.timberta.walletleaks.presentation.base.BaseViewModel
+import com.timberta.walletleaks.presentation.models.toUI
 
-class CoinListViewModel : BaseViewModel() {
+class CoinListViewModel(private val fetchCoinsUseCase: FetchCoinsUseCase) : BaseViewModel() {
+
+    fun fetchCoins() =
+        fetchCoinsUseCase().gatherPagingRequest { it.toUI() }
+
+
+    init {
+        fetchCoins()
+    }
 }
