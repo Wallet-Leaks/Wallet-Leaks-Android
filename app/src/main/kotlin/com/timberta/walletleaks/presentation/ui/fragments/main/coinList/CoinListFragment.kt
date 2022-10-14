3e4bc7b4299341b73c7361a28eea505183a8a430
Clar1fy <timplifier@gmail.com>
2022-10-14 23:15:53 +0600

    refactored coinlistFragment,added necessary adapter logic

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/coinList/CoinListFragment.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/coinList/CoinListFragment.kt
@@ -1,17 +1,42 @@
 package com.timberta.walletleaks.presentation.ui.fragments.main.coinList
 
-import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timberta.walletleaks.R
+import com.timberta.walletleaks.data.local.preferences.UserDataPreferencesManager
 import com.timberta.walletleaks.databinding.FragmentCoinListBinding
 import com.timberta.walletleaks.presentation.base.BaseFragment
-import org.koin.dsl.module
+import com.timberta.walletleaks.presentation.ui.adapters.CoinListAdapter
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class CoinListFragment :
     BaseFragment<FragmentCoinListBinding, CoinListViewModel>(R.layout.fragment_coin_list) {
 
     override val binding by viewBinding(FragmentCoinListBinding::bind)
-    override val viewModel by viewModels<CoinListViewModel>()
+    override val viewModel by viewModel<CoinListViewModel>()
 
+    private val userDataPreferencesManager: UserDataPreferencesManager by inject()
+    private val coinListAdapter =
+        CoinListAdapter(userDataPreferencesManager.doesUserHavePremium)
+
+    override fun initialize() {
+        constructAdapter()
+    }
+
+    private fun constructAdapter() {
+        binding.rvCoinList.adapter = coinListAdapter
+        binding.rvCoinList.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun launchObservers() {
+        subscribeToCoinList()
+    }
+
+    private fun subscribeToCoinList() {
+        viewModel.fetchCoins().spectatePaging {
+            coinListAdapter.submitData(it)
+        }
+    }
 }
