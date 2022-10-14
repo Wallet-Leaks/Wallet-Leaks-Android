3a07caf96b4aee950cfd2be7ba653e5929bfed27
Clar1fy <timplifier@gmail.com>
2022-10-14 23:27:38 +0600

    feat(di): added di package with ViewModelModule

---
+++ app/src/main/kotlin/com/timberta/walletleaks/di/ViewModelModule.kt
@@ -0,0 +1,12 @@
+package com.timberta.walletleaks.di
+
+import com.timberta.walletleaks.presentation.ui.fragments.main.coinList.CoinListViewModel
+import com.timberta.walletleaks.presentation.ui.fragments.main.coinList.selectCoins.SelectCoinsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel {
+        CoinListViewModel(get())
+    }
+}
