751cb64e9f26993b2d3a9f87cd6638916f5fa438
albertik001 <albert.nurlanovv@mail.ru>
2022-10-15 00:34:43 +0600

    algorithm crypto bot done

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeFragment.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeFragment.kt
@@ -4,14 +4,17 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.lifecycle.viewModelScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timberta.walletleaks.R
 import com.timberta.walletleaks.databinding.FragmentHomeBinding
 import com.timberta.walletleaks.presentation.base.BaseFragment
+import com.timberta.walletleaks.presentation.extensions.invisible
+import com.timberta.walletleaks.presentation.extensions.visible
 import com.timberta.walletleaks.presentation.ui.adapters.CryptoAlgorithmAdapter
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
@@ -21,32 +24,71 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
     private val adapter = CryptoAlgorithmAdapter()
 
     override fun initialize() {
+        viewModel.processCryptoWorkState.value = false
         binding.rvCryptoOperationHome.adapter = adapter
+        containerOperationIsVisible()
     }
 
     override fun constructListeners() {
         binding.btnStartOperation.setOnClickListener {
+            viewModel.processCryptoWorkState.value = true
             viewLifecycleOwner.lifecycleScope.launch {
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    for (i in 1..10000) {
-                        delay(300)
-                        viewModel.searchCryptoWallets(i)
-                        adapter.notifyItemInserted(i)
-                        if (adapter.itemCount > 0) {
-                            binding.rvCryptoOperationHome.scrollToPosition(adapter.itemCount - 1)
+                    for (i in viewModel.processIndex..10000) {
+                        if (viewModel.processCryptoWorkState.value) {
+                            viewModel.processIndex = adapter.itemCount
+                            viewModel.searchCryptoWallets()
+                            adapter.notifyItemInserted(viewModel.processIndex)
+                            updateAdapterScroll()
+                            binding.price.text =
+                                String.format("%.4f", viewModel.allPrice * 19147.50)
+                            viewModel.processIndex = i
+                        } else {
+                            break
                         }
-                        binding.price.text = String.format("%.4f", viewModel.allPrice)
+                    }
+                }
+            }
+        }
+        binding.btnStopSearch.setOnClickListener {
+            viewModel.processCryptoWorkState.value = false
+        }
+    }
+
+    override fun launchObservers() {
+        collectCryptoAlgorithm()
+    }
+
+    private fun collectCryptoAlgorithm() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getListCryptoWalletsState.collect {
+                    adapter.submitList(it)
+                }
+            }
+        }
+    }
+
+    private fun containerOperationIsVisible() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.processCryptoWorkState.collectLatest {
+                    if (it) {
+                        binding.containerInStartOperation.invisible()
+                        binding.containerInOperation.visible()
+                    } else {
+                        binding.containerInStartOperation.visible()
+                        binding.containerInOperation.invisible()
                     }
                 }
             }
         }
     }
 
-    override fun launchObservers() {
-        viewModel.viewModelScope.launch {
-            viewModel.getListCryptoWalletsState.collect {
-                adapter.submitList(it)
-            }
+    private fun updateAdapterScroll() {
+        if (adapter.itemCount > 0) {
+            binding.rvCryptoOperationHome.scrollToPosition(adapter.itemCount - 1)
         }
     }
+
 }
