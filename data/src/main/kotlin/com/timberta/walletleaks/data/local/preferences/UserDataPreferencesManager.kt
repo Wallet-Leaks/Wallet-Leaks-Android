2730616c4c245b15e922179b5ac74dd006226735
Clar1fy <timplifier@gmail.com>
2022-10-23 02:23:48 +0600

    added hasNonPremiumUserTriedToSelectMultipleCoins boolean and method to do something if false

--- data/src/main/kotlin/com/timberta/walletleaks/data/local/preferences/UserDataPreferencesManager.kt
+++ data/src/main/kotlin/com/timberta/walletleaks/data/local/preferences/UserDataPreferencesManager.kt
@@ -2,14 +2,26 @@ package com.timberta.walletleaks.data.local.preferences
 
 import android.content.SharedPreferences
 import com.timberta.walletleaks.data.local.preferences.PreferencesKeys.DOES_USER_HAVE_PREMIUM
+import com.timberta.walletleaks.data.local.preferences.PreferencesKeys.HAS_NON_PREMIUM_USER_TRIED_TO_SELECT_MULTIPLE_COINS
 import com.timberta.walletleaks.data.local.preferences.PreferencesKeys.IS_USER_AUTHENTICATED
 
 class UserDataPreferencesManager constructor(private val preferences: SharedPreferences) {
     var isAuthenticated: Boolean
         get() = preferences.getBoolean(IS_USER_AUTHENTICATED, false)
-        set(value) = preferences.edit().putBoolean(IS_USER_AUTHENTICATED, value).apply()
+        set(value) = preferences.put(IS_USER_AUTHENTICATED, value)
 
     var doesUserHavePremium: Boolean
-        get() = preferences.getBoolean(DOES_USER_HAVE_PREMIUM, true)
-        set(value) = preferences.edit().putBoolean(DOES_USER_HAVE_PREMIUM, value).apply()
+        get() = preferences.getBoolean(DOES_USER_HAVE_PREMIUM, false)
+        set(value) = preferences.put(DOES_USER_HAVE_PREMIUM, value)
+
+    var hasNonPremiumUserTriedToSelectMultipleCoins: Boolean
+        get() = preferences.getBoolean(HAS_NON_PREMIUM_USER_TRIED_TO_SELECT_MULTIPLE_COINS, false)
+        set(value) = preferences.put(HAS_NON_PREMIUM_USER_TRIED_TO_SELECT_MULTIPLE_COINS, value)
+
+    fun actionIfNonPremiumUserSelectsMultipleCoinsForTheFirstTime(action: () -> Unit) {
+        if (!hasNonPremiumUserTriedToSelectMultipleCoins && !doesUserHavePremium) {
+            action()
+            hasNonPremiumUserTriedToSelectMultipleCoins = true
+        }
+    }
 }
