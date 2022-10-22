aaa7a13fa4289f4982e1f996db567de09264bf94
albertik001 <albert.nurlanovv@mail.ru>
2022-10-23 01:43:50 +0600

    timer user mining

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeViewModel.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/home/HomeViewModel.kt
@@ -1,11 +1,13 @@
 package com.timberta.walletleaks.presentation.ui.fragments.main.home
 
+import android.os.CountDownTimer
 import androidx.lifecycle.viewModelScope
 import com.timberta.walletleaks.presentation.base.BaseViewModel
 import com.timberta.walletleaks.presentation.ui.model.CryptoWalletUI
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.util.*
 import kotlin.random.Random
 
 class HomeViewModel : BaseViewModel() {
@@ -13,7 +15,14 @@ class HomeViewModel : BaseViewModel() {
     private val modelList = ArrayList<CryptoWalletUI>()
     private val _getListCryptoWalletsState = MutableStateFlow<List<CryptoWalletUI>>(emptyList())
     val getListCryptoWalletsState = _getListCryptoWalletsState.asStateFlow()
+
+    private val _getTimeTimerText = MutableStateFlow("")
+    val getTimeTimerText = _getTimeTimerText.asStateFlow()
+
     val processCryptoWorkState = MutableStateFlow(false)
+    private var countDownTimer: CountDownTimer? = null
+    private var mTimeInMillis: Long = 0
+
     var allPrice = 0.0
     var processIndex = 0
 
@@ -36,5 +45,31 @@ class HomeViewModel : BaseViewModel() {
     private fun generateString(length: Int): String {
         val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
         return (1..length).map { allowedChars.random() }.joinToString("")
+    }
+
+    fun startTimer(timeInMillis: Long) {
+        countDownTimer?.cancel()
+        countDownTimer = object : CountDownTimer(timeInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                mTimeInMillis = millisUntilFinished
+                updateCountDownText()
+            }
+
+            override fun onFinish() {
+                updateCountDownText()
+            }
+        }.start()
+    }
+
+    fun pauseTimer() {
+        countDownTimer?.cancel()
+    }
+
+    fun updateCountDownText() {
+        val hours = (mTimeInMillis / (1000 * 60 * 60) % 24)
+        val minutes = (mTimeInMillis / (1000 * 60)) % 60
+        val seconds = (mTimeInMillis / 1000) % 60
+        _getTimeTimerText.value =
+            String.format(Locale.getDefault(), "%01d:%02d:%02d", hours, minutes, seconds)
     }
 }
