dded9631c54a20a8ea51302c11525489f5f4f235
Clar1fy <timplifier@gmail.com>
2022-10-23 02:23:20 +0600

    changed any? with String?

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/UIExtensions.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/UIExtensions.kt
@@ -2,13 +2,5 @@ package com.timberta.walletleaks.presentation.extensions
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
-fun ImageView.loadImageWithGlide(url: Any?) = Glide.with(this).load(url).centerCrop().into(this)
-fun ImageView.setImage(uri: String) {
-    Glide.with(this)
-        .load(uri)
-        .centerCrop()
-        .transition(DrawableTransitionOptions.withCrossFade())
-        .into(this)
-}
+fun ImageView.loadImageWithGlide(url: String?) = Glide.with(this).load(url).centerCrop().into(this)
