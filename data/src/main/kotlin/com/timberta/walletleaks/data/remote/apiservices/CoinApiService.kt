319b00bf7c851ced620ce1b184aabd70c94600ca
Clar1fy <timplifier@gmail.com>
2022-10-23 01:45:23 +0600

    changed request route and added page as query

--- data/src/main/kotlin/com/timberta/walletleaks/data/remote/apiservices/CoinApiService.kt
+++ data/src/main/kotlin/com/timberta/walletleaks/data/remote/apiservices/CoinApiService.kt
@@ -1,10 +1,11 @@
 package com.timberta.walletleaks.data.remote.apiservices
 
-import com.timberta.walletleaks.data.remote.dtos.BasePagingResponse
+import com.timberta.walletleaks.data.base.BasePagingResponse
 import com.timberta.walletleaks.data.remote.dtos.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CoinApiService {
-    @GET("api/v1/wallets")
-    suspend fun fetchCoins(): BasePagingResponse<CoinDto>
+    @GET("api/v1/wallets/wallet")
+    suspend fun fetchCoins(@Query("page") page: Int): BasePagingResponse<CoinDto>
 }
