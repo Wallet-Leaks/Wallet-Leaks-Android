cae64445c5ddcdb04387ec7a2579bdd4ebe3d445
albertik001 <albert.nurlanovv@mail.ru>
2022-10-23 01:44:57 +0600

    timer minig

--- data/src/main/kotlin/com/timberta/walletleaks/data/local/preferences/PreferencesKeys.kt
+++ data/src/main/kotlin/com/timberta/walletleaks/data/local/preferences/PreferencesKeys.kt
@@ -3,4 +3,5 @@ package com.timberta.walletleaks.data.local.preferences
 object PreferencesKeys {
     const val DOES_USER_HAVE_PREMIUM = "doesUserHavePremium"
     const val IS_USER_AUTHENTICATED = "isUserAuthenticated"
+    const val MINING_TIME_TIMER = "miningTimeTimer"
 }
