23330294d3cfb16e6efd2552bd7396ad1022c1b9
albertik001 <albert.nurlanovv@mail.ru>
2022-11-02 16:52:17 +0600

    rename viewmodel

---
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/profile/settings/ProfileSettingsViewModel.kt
@@ -0,0 +1,6 @@
+package com.timberta.walletleaks.presentation.ui.fragments.main.profile.settings
+
+import com.timberta.walletleaks.presentation.base.BaseViewModel
+
+class ProfileSettingsViewModel : BaseViewModel() {
+}
