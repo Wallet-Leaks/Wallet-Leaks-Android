675fdd23c1cb65879e31f945404255729399ec7b
Clar1fy <timplifier@gmail.com>
2022-11-06 04:34:55 +0600

    feat(Data/Domain/Presentation): added a authenticationRepository to authenticate user with email, password and confirmation password ,added model that authentication api service returns

---
+++ domain/src/main/kotlin/com/timberta/walletleaks/domain/repositories/AuthenticationRepository.kt
@@ -0,0 +1,13 @@
+package com.timberta.walletleaks.domain.repositories
+
+import com.timberta.walletleaks.domain.either.Either
+import com.timberta.walletleaks.domain.models.TokensModel
+import kotlinx.coroutines.flow.Flow
+
+interface AuthenticationRepository {
+    fun signUp(
+        username: String,
+        password: String,
+        confirmationPassword: String
+    ): Flow<Either<String, TokensModel>>
+}
