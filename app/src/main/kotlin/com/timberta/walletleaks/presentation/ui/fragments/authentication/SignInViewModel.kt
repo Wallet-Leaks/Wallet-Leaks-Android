f13f9b803419859ab60a37a9969722923fe74f5a
Clar1fy <timplifier@gmail.com>
2022-11-06 04:49:22 +0600

    feat(Authentication): linked with backend, added an authentication logic

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/authentication/SignInViewModel.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/authentication/SignInViewModel.kt
@@ -1,7 +1,23 @@
 package com.timberta.walletleaks.presentation.ui.fragments.authentication
 
+import com.timberta.walletleaks.domain.useCases.SignUpUseCase
 import com.timberta.walletleaks.presentation.base.BaseViewModel
+import com.timberta.walletleaks.presentation.models.TokensUI
+import com.timberta.walletleaks.presentation.models.toUI
+import com.timberta.walletleaks.presentation.ui.model.TokensUI
+import com.timberta.walletleaks.presentation.ui.model.toUI
+import kotlinx.coroutines.flow.asStateFlow
 
-class SignInViewModel : BaseViewModel() {
+class SignInViewModel(
+    private val signUpUseCase: SignUpUseCase
+) : BaseViewModel() {
+    private val _signUpState = mutableUiStateFlow<TokensUI>()
+    val signUpState = _signUpState.asStateFlow()
 
+    fun signUp(username: String, password: String, confirmedPassword: String) =
+        signUpUseCase(
+            username,
+            password,
+            confirmedPassword
+        ).gatherRequest(_signUpState) { it.toUI() }
 }
