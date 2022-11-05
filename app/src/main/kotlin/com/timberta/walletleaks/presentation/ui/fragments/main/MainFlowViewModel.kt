47a9d10cb701f45a9fe13f730d7dce3bcdadfe4b
Clar1fy <timplifier@gmail.com>
2022-11-06 04:46:05 +0600

    feat(MainFlowFragment.kt): logic to fetch user every 10 seconds and 1 minute after the initial fetch; show dialog if he is not verified

---
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/MainFlowViewModel.kt
@@ -0,0 +1,20 @@
+package com.timberta.walletleaks.presentation.ui.fragments.main
+
+import com.timberta.walletleaks.data.local.preferences.UserDataPreferencesManager
+import com.timberta.walletleaks.domain.useCases.FetchUserUseCase
+import com.timberta.walletleaks.presentation.base.BaseViewModel
+import com.timberta.walletleaks.presentation.models.UserUI
+import com.timberta.walletleaks.presentation.models.toUI
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainFlowViewModel(
+    private val fetchUserUseCase: FetchUserUseCase,
+    private val userDataPreferencesManager: UserDataPreferencesManager
+) : BaseViewModel() {
+
+    private val _userState = mutableUiStateFlow<UserUI>()
+    val userState = _userState.asStateFlow()
+
+    fun fetchUser() =
+        fetchUserUseCase(userDataPreferencesManager.userId.toString()).gatherRequest(_userState) { it.toUI() }
+}
