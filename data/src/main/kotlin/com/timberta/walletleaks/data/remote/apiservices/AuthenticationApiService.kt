21f37061d37d32852c15dda651cd80062238fcd1
Clar1fy <timplifier@gmail.com>
2022-11-06 04:28:01 +0600

    feat(Data/Remote): added an AuthenticationApiService to authenticate user, generated it in network client

---
+++ data/src/main/kotlin/com/timberta/walletleaks/data/remote/apiservices/AuthenticationApiService.kt
@@ -0,0 +1,12 @@
+package com.timberta.walletleaks.data.remote.apiservices
+
+import com.timberta.walletleaks.data.remote.dtos.SignUpDto
+import com.timberta.walletleaks.data.remote.dtos.TokensDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthenticationApiService {
+    @POST("api/v1/users/register/")
+    suspend fun signUp(@Body signUpDto: SignUpDto): TokensDto
+
+}
--- domain/src/main/kotlin/com/timberta/walletleaks/domain/module/DomainModule.kt
+++
@@ -1,1 +0,0 @@
-package com.timberta.walletleaks.domain.module
