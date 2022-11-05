bc7977904c12ed918b14e841a8abdfc75af3293a
Clar1fy <timplifier@gmail.com>
2022-11-06 04:47:27 +0600

    feat(PreferencesKeys.kt,-UserDataPreferencesManager.kt): major changes: multiple keys deployment, multiple variables initialization

--- data/src/main/kotlin/com/timberta/walletleaks/data/local/preferences/PreferencesKeys.kt
+++ data/src/main/kotlin/com/timberta/walletleaks/data/local/preferences/PreferencesKeys.kt
@@ -1,8 +1,11 @@
 package com.timberta.walletleaks.data.local.preferences
 
 object PreferencesKeys {
-    const val DOES_USER_HAVE_PREMIUM = "doesUserHavePremium"
     const val IS_USER_AUTHENTICATED = "isUserAuthenticated"
+    const val ACCESS_TOKEN = "accessToken"
+    const val REFRESH_TOKEN = "refreshToken"
+    const val USER_ID = "userId"
+    const val DOES_USER_HAVE_PREMIUM = "doesUserHavePremium"
     const val HAS_NON_PREMIUM_USER_TRIED_TO_SELECT_MULTIPLE_COINS =
         "hasNonPremiumUserTriedToSelectMultipleCoins"
     const val MINING_TIME_TIMER = "miningTimeTimer"
