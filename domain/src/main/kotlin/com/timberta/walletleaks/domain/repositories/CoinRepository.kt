cc38a013fa9dad477b72d0d253e3b69dec85970a
Clar1fy <timplifier@gmail.com>
2022-11-25 14:24:48 +0600

    feat(Data/Domain): added a feature to fetch only certain coins by their id

--- domain/src/main/kotlin/com/timberta/walletleaks/domain/repositories/CoinRepository.kt
+++ domain/src/main/kotlin/com/timberta/walletleaks/domain/repositories/CoinRepository.kt
@@ -1,9 +1,11 @@
 package com.timberta.walletleaks.domain.repositories
 
 import androidx.paging.PagingData
+import com.timberta.walletleaks.domain.either.Either
 import com.timberta.walletleaks.domain.models.CoinModel
 import kotlinx.coroutines.flow.Flow
 
 interface CoinRepository {
     fun fetchCoins(): Flow<PagingData<CoinModel>>
+    fun fetchCertainCoins(coinIds: String): Flow<Either<String, List<CoinModel>>>
 }
