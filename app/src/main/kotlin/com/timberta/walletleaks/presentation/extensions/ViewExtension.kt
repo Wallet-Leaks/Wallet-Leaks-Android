8d3effc49b1d9236934d6b08403e98bdedf56019
Clar1fy <timplifier@gmail.com>
2022-12-04 11:53:13 +0600

    feat(Extensions/ViewExtension.kt): added an extensions to bind shimmer to loading state of the app

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/ViewExtension.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/ViewExtension.kt
@@ -3,6 +3,7 @@ package com.timberta.walletleaks.presentation.extensions
 import android.view.View
 import android.widget.ProgressBar
 import androidx.core.view.isVisible
+import com.facebook.shimmer.ShimmerFrameLayout
 import com.timberta.walletleaks.presentation.ui.state.UIState
 
 fun View.visible() {
@@ -25,4 +26,14 @@ fun makeMultipleViewsInvisible(vararg views: View) {
 
 fun <T> ProgressBar.bindToUIStateLoading(uiState: UIState<T>) {
     isVisible = uiState is UIState.Loading
+}
+
+fun <T> ShimmerFrameLayout.bindToUIStateLoading(uiState: UIState<T>) {
+    when (uiState) {
+        is UIState.Loading -> startShimmer()
+        else -> {
+            stopShimmer()
+            hideShimmer()
+        }
+    }
 }
