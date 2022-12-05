bde063ec06e8f520c501ceb496c26618aaf24c71
Clar1fy <timplifier@gmail.com>
2022-12-05 16:15:38 +0600

    feat(Presentaiton/UI/Fragments/Main/Profile/Withdrawal/Confirmation/WithdrawalConfirmationViewModel.kt): added use case method and state to spectate

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/profile/withdrawal/confirmation/WithdrawalConfirmationViewModel.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/ui/fragments/main/profile/withdrawal/confirmation/WithdrawalConfirmationViewModel.kt
@@ -1,6 +1,20 @@
 package com.timberta.walletleaks.presentation.ui.fragments.main.profile.withdrawal.confirmation
 
+import com.timberta.walletleaks.domain.useCases.ModifyUserInfoUseCase
 import com.timberta.walletleaks.presentation.base.BaseViewModel
+import com.timberta.walletleaks.presentation.models.GeneralUserInfoUI
+import com.timberta.walletleaks.presentation.models.toDomain
+import kotlinx.coroutines.flow.asStateFlow
 
-class WithdrawalConfirmationViewModel : BaseViewModel() {
+class WithdrawalConfirmationViewModel(
+    private val modifyUserInfoUseCase: ModifyUserInfoUseCase
+) :
+    BaseViewModel() {
+    private val _userBalanceModificationState = mutableUiStateFlow<Unit>()
+    val userBalanceModificationState = _userBalanceModificationState.asStateFlow()
+
+    fun modifyUserBalance(generalUserInfoUI: GeneralUserInfoUI) =
+        modifyUserInfoUseCase(generalUserInfoUI.toDomain()).gatherRequest(
+            _userBalanceModificationState
+        )
 }
