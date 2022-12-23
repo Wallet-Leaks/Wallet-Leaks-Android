bbd93c01f1409676768b6994b89ae9c4050c421d
Clar1fy <timplifier@gmail.com>
2022-12-23 18:22:18 +0600

    feat(Presentation/Extensions/MiscellaneousExtensions.kt): cleaned up
    n

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/MiscellaneousExtensions.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/MiscellaneousExtensions.kt
@@ -5,11 +5,5 @@ import android.os.Looper
 import android.util.Log
 
 fun loge(msg: String, value: Any? = null) = Log.e("fuck", msg + value)
-fun postHandler(func: () -> Unit) =
-    Handler(Looper.myLooper()!!).post { func() }
-
-/**
- * Extension function for posting a delayed [Handler]
- * */
 fun postHandler(delay: Long, func: () -> Unit) =
     Handler(Looper.myLooper()!!).postDelayed(func, delay)
