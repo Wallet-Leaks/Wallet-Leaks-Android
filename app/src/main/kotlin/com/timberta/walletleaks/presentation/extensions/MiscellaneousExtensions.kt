43b80988b58d309486bc6efb4a922316a8bf01f4
Clar1fy <timplifier@gmail.com>
2022-12-23 17:45:29 +0600

    feat(Extensions/MiscellaneousExtensions.kt): extension to invoke post handler

--- app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/MiscellaneousExtensions.kt
+++ app/src/main/kotlin/com/timberta/walletleaks/presentation/extensions/MiscellaneousExtensions.kt
@@ -1,5 +1,15 @@
 package com.timberta.walletleaks.presentation.extensions
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 
 fun loge(msg: String, value: Any? = null) = Log.e("fuck", msg + value)
+fun postHandler(func: () -> Unit) =
+    Handler(Looper.myLooper()!!).post { func() }
+
+/**
+ * Extension function for posting a delayed [Handler]
+ * */
+fun postHandler(delay: Long, func: () -> Unit) =
+    Handler(Looper.myLooper()!!).postDelayed(func, delay)
