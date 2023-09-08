1c08df4859b908a48c5d953ae6793667808747d0
timplifier <timplifier@gmail.com>
2023-09-09 00:56:27 +0600

    feat(app/build.gradle.kts): migrated to version catalog, count build count

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,60 @@
+import java.util.Properties
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    libs.plugins.apply {
+        id(agp.application.get().pluginId)
+        id(kotlin.android.get().pluginId)
+        id(google.services.get().pluginId)
+        id(triplet.playpublisher.get().pluginId) version (libs.versions.triplet.playpublisher.get())
+    }
 }
 
 android {
-    namespace = "org.tbm.walletleaks"
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "org.tbm.walletleaks"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
+    androidProjectConfig.versions.apply {
+        namespace = namespaces.android.app.get()
+        compileSdk = sdk.compile.get().toInt()
+        defaultConfig {
+            applicationId =
+                namespaces.android.app.get().replace(".android", "")
+            minSdk = sdk.min.get().toInt()
+            targetSdk = sdk.target.get().toInt()
+            val version =
+                "${versioning.major.get()}.${versioning.feature.get()}.${versioning.patch.get()}"
+            versionCode =
+                version.replace(".", "").toInt()
+            versionName =
+                version
+        }
+        buildFeatures {
+            compose = true
+        }
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.versions.kotlin.compiler.compose.get()
+        }
+        packaging {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            }
+        }
+        buildTypes {
+            release {
+                isDebuggable = false
+                isShrinkResources = true
+                isMinifyEnabled = true
+            }
+            debug {
+                isDebuggable = true
+                isShrinkResources = false
+                isMinifyEnabled = false
+            }
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
     }
     kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        jvmTarget = gradleProjectConfig.versions.kotlin.options.jvm.get()
     }
 }
 
@@ -63,4 +75,35 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+tasks.register("updateBuildCount") {
+    doLast {
+        val readmeFile = File(project.rootDir, "README.md")
+        var buildCount = 0
+
+        val readmeContents = readmeFile.readText()
+        val buildCountPattern = """Total build count: (\d+)""".toRegex()
+        val matchResult = buildCountPattern.find(readmeContents)
+
+        matchResult?.let {
+            buildCount = matchResult.groupValues[1].toIntOrNull() ?: 0
+        }
+
+        buildCount++
+        readmeFile.writeText(
+            readmeContents.replaceFirst(
+                buildCountPattern,
+                "Total build count: $buildCount"
+            )
+        )
+    }
+}
+
+pluginManager.withPlugin("com.android.application") {
+    tasks.whenTaskAdded {
+        if (name.startsWith("assemble")) {
+            finalizedBy("updateBuildCount")
+        }
+    }
 }
