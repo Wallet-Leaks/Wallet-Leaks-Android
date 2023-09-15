41a8b41b10057574780123afafec63ffce776c7c
timplifier <timplifier@gmail.com>
2023-09-15 15:49:24 +0600

    feat(build.gradle.kts): added kotlin compile task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.agp.application) apply false
     alias(libs.plugins.agp.library) apply false
@@ -8,4 +10,7 @@ plugins {
 
 tasks.register(gradleProjectConfig.versions.tasks.clean.get(), Delete::class) {
     delete(rootProject.buildDir)
+}
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions { jvmTarget = gradleProjectConfig.versions.kotlin.options.jvm.get() }
 }
