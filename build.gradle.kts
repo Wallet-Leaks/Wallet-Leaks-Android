b9d2c64a9875ea9f4eee1a90a69aa460e63ff3c7
timplifier <timplifier@gmail.com>
2023-09-21 20:55:23 +0600

    feat(build.gradle.kts): added more plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     alias(libs.plugins.agp.application) apply false
     alias(libs.plugins.agp.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.google.services) apply false
 }
 
 tasks.register(gradleProjectConfig.versions.tasks.clean.get(), Delete::class) {
     delete(rootProject.buildDir)
-}
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions { jvmTarget = gradleProjectConfig.versions.kotlin.options.jvm.get() }
 }
