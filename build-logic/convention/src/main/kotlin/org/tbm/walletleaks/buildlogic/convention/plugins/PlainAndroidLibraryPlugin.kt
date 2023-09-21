cb8768cb2a1af430dab9e6eabd2b77d84248b2f5
timplifier <timplifier@gmail.com>
2023-09-21 20:50:32 +0600

    feat(plainandroidlibraryplugin): added plain android library plugin

---
+++ build-logic/convention/src/main/kotlin/org/tbm/walletleaks/buildlogic/convention/plugins/PlainAndroidLibraryPlugin.kt
@@ -0,0 +1,5 @@
+package org.tbm.walletleaks.buildlogic.convention.plugins
+
+import org.tbm.walletleaks.buildlogic.convention.plugins.base.AndroidLibraryPlugin
+
+internal class PlainAndroidLibraryPlugin : AndroidLibraryPlugin()
