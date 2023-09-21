c2a2693883c5f981fcd5958adce1641d678dd07b
timplifier <timplifier@gmail.com>
2023-09-21 20:57:07 +0600

    feat(app/build.gradle.kts): imported everything

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import java.util.Properties
 plugins {
     libs.plugins.apply {
         id(agp.application.get().pluginId)
@@ -10,11 +9,11 @@ plugins {
 
 android {
     androidProjectConfig.versions.apply {
-        namespace = namespaces.android.app.get()
+        namespace = namespaces.common.get()
         compileSdk = sdk.compile.get().toInt()
         defaultConfig {
             applicationId =
-                namespaces.android.app.get().replace(".android", "")
+                namespaces.common.get()
             minSdk = sdk.min.get().toInt()
             targetSdk = sdk.target.get().toInt()
             val version =
@@ -59,22 +58,9 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(projects.feature.authentication.presentation)
+    implementation(projects.feature.main.presentation)
+    implementation(libs.bundles.androidx.compose)
 }
 
 tasks.register("updateBuildCount") {
