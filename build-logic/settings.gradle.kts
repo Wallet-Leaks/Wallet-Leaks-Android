adb2572876805d1d8bb4b31d32e08c97a3bbbe4e
timplifier <timplifier@gmail.com>
2023-09-21 21:02:00 +0600

    feat(build-logic/settings.gradle.kts): deleted typesafe project accessors, added version catalog

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -5,6 +5,7 @@ dependencyResolutionManagement {
         gradlePluginPortal()
     }
     versionCatalogs {
+
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
         }
